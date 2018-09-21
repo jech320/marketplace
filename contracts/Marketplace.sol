311d679881dbf0675bd86e79d3c6f9e27829a910
Jechris Soldevilla <jech320@gmail.com>
2018-09-21 18:06:48 +0800

    Added events and logging to state changing functions

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,10 +4,52 @@ import { Ownable } from "./Ownable.sol";
 import { SafeMathLib } from "./SafeMathLib.sol";
 
 contract Marketplace is Ownable {
+    event LogUserRegistration(
+      address indexed userAddress,
+      uint role,
+      string contactEmail,
+      string contactNumber
+    );
+    event LogItemForSaleAddition(
+      address indexed sellerAddress,
+      string name,
+      string description,
+      uint price
+    );
+    event LogItemForSaleImageAddition(
+      address indexed sellerAddress,
+      uint index,
+      string imageIpfsHash
+    );
+    event LogItemForSaleUpdate(
+      address indexed sellerAddress,
+      uint index,
+      string name,
+      string description,
+      uint price
+    );
+    event LogItemForSaleDeletion(
+      address indexed userAddress,
+      string name,
+      string description,
+      uint price
+    );
+    event LogItemForSaleImageDeletion(
+      address indexed sellerAddress,
+      string imageIpfsHash
+    );
+    event LogItemPurchase(
+      address indexed buyerAddress,
+      address sellerAddress,
+      string name,
+      string description,
+      uint price
+    );
+
     enum Role { Buyer, Seller }
     string[] public roles;
-    mapping (address => User) private users;
     address[] public userAddressIndices;
+    mapping (address => User) private users;
     
     constructor() public {
         roles.push("Buyer");
@@ -57,12 +99,26 @@ contract Marketplace is Ownable {
             || isSeller(msg.sender), "Error: Not allowed to view");
         _;
     }
+
+    function isSeller(address addr) public view returns (bool) {
+        return users[addr].role == Role.Seller;
+    }
+    
+    function isBuyer(address addr) public view returns (bool) {
+        return users[addr].role == Role.Buyer;
+    }
+    
+    function isMatch(address addr1, address addr2) public pure returns (bool) {
+        return addr1 == addr2;
+    }
     
     function registerUser(Role role, string email, string number) public {
         users[msg.sender].role = role;
         users[msg.sender].contact.email = email;
         users[msg.sender].contact.number = number;
         userAddressIndices.push(msg.sender);
+        
+        emit LogUserRegistration(msg.sender, uint(role), email, number);
     }
     
     function addItemForSale(string name, string description, uint price)
@@ -78,6 +134,13 @@ contract Marketplace is Ownable {
         });
 
         users[msg.sender].itemsForSale.push(item);
+
+        emit LogItemForSaleAddition(
+          msg.sender,
+          name,
+          description,
+          price
+        );
     }
     
     function addImageForItemForSale(uint index, string imageIpfsHash)
@@ -85,6 +148,12 @@ contract Marketplace is Ownable {
         onlySeller
     {
         users[msg.sender].itemsForSale[index].imageIpfsHashes.push(imageIpfsHash);
+
+        emit LogItemForSaleImageAddition(
+          msg.sender,
+          index,
+          imageIpfsHash
+        );
     }
     
     function getItemForSaleCount(address sellerAddress)
@@ -150,6 +219,14 @@ contract Marketplace is Ownable {
         sellerAddress.transfer(item.price);
         removeItemForSale(sellerAddress, itemIndex);
         users[msg.sender].itemsBought.push(item);
+
+        emit LogItemPurchase(
+          msg.sender,
+          sellerAddress,
+          item.name,
+          item.description,
+          item.price
+        );
     }
     
     function removeItemForSaleBySeller(uint index) public onlySeller {
@@ -159,7 +236,16 @@ contract Marketplace is Ownable {
     function removeItemForSale(address sellerAddress, uint index)
         private
     {
-        uint  arrLength = users[sellerAddress].itemsForSale.length;
+        Item memory item = users[sellerAddress].itemsForSale[index];
+
+        emit LogItemForSaleDeletion(
+          msg.sender,
+          item.name,
+          item.description,
+          item.price
+        );
+
+        uint arrLength = users[sellerAddress].itemsForSale.length;
         users[sellerAddress].itemsForSale[index] = users[sellerAddress].itemsForSale[arrLength - 1];
         users[sellerAddress].itemsForSale.length--;
     }
@@ -173,12 +259,24 @@ contract Marketplace is Ownable {
         users[msg.sender].itemsForSale[index].name = name;
         users[msg.sender].itemsForSale[index].description = description;
         users[msg.sender].itemsForSale[index].price = price;
+    
+        emit LogItemForSaleUpdate(
+          msg.sender,
+          index,
+          name,
+          description,
+          price
+        );
     }
     
     function removeItemForSaleImage(uint itemIndex, uint imageIndex)
         public
         onlySeller
     {
+        string memory imageIpfsHash = users[msg.sender].itemsForSale[itemIndex].imageIpfsHashes[imageIndex];
+
+        emit LogItemForSaleImageDeletion(msg.sender, imageIpfsHash);
+
         uint arrLength = users[msg.sender].itemsForSale[itemIndex].imageIpfsHashes.length;
         string memory lastImageHash = users[msg.sender].itemsForSale[itemIndex].imageIpfsHashes[arrLength - 1];
         users[msg.sender].itemsForSale[itemIndex].imageIpfsHashes[imageIndex] = lastImageHash;
@@ -193,18 +291,6 @@ contract Marketplace is Ownable {
     {
         Contact memory sellerContact = users[sellerAddress].contact;
         return (sellerContact.email, sellerContact.number);
-    }
-    
-    function isSeller(address addr) public view returns (bool) {
-        return users[addr].role == Role.Seller;
-    }
-    
-    function isBuyer(address addr) public view returns (bool) {
-        return users[addr].role == Role.Buyer;
-    }
-    
-    function isMatch(address addr1, address addr2) public pure returns (bool) {
-        return addr1 == addr2;
     }
     
     function getItemBought(address buyerAddress, uint index)
