cf9533afaa07f04d795f343a334007fb5c03c3cb
Jechris Soldevilla <jech320@gmail.com>
2018-09-24 17:04:05 +0800

    Added divide function and refactored names

--- contracts/SafeMathLib.sol
+++ contracts/SafeMathLib.sol
@@ -1,20 +1,25 @@
 pragma solidity ^0.4.24;
 
 library SafeMathLib {
-    function times(uint a, uint b) public pure returns (uint) {
+    function multiply(uint a, uint b) public pure returns (uint) {
         uint c = a * b;
         require(a == 0 || c / a == b);
         return c;
     }
 
-    function minus(uint a, uint b) public pure returns (uint) {
+    function subtract(uint a, uint b) public pure returns (uint) {
         require(b <= a);
         return a - b;
     }
 
-    function plus(uint a, uint b) public pure returns (uint) {
+    function add(uint a, uint b) public pure returns (uint) {
         uint c = a + b;
         require(c>=a && c>=b);
+        return c;
+    }
+    
+    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a / b;
         return c;
     }
 }
