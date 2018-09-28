1a07e172e413e5c6c2774e3ed011608380277b7f
Jechris Soldevilla <jech320@gmail.com>
2018-09-28 19:30:29 +0800

    Modified test to cover modified contract

--- test/TestMarketplace.js
+++ test/TestMarketplace.js
@@ -1,15 +1,9 @@
 const Marketplace = artifacts.require("Marketplace");
 
-const FINNEY = 10**15;
+const FINNEY = 10 ** 15;
 
 contract("Marketplace", accounts => {
-  const [
-    owner,
-    seller,
-    buyer,
-    buyer2,    
-    visitor
-  ] = accounts;
+  const [owner, seller, buyer, buyer2, visitor] = accounts;
   let marketplace;
   const item = {
     name: "Shoes",
@@ -36,28 +30,47 @@ contract("Marketplace", accounts => {
     });
     const itemForSaleCount = await marketplace.getItemForSaleCount(seller);
     const itemIndex = itemForSaleCount.toNumber() - 1;
-    await marketplace.addImageForItemForSale(itemIndex, item.imageIpfsHashes[0], {
-      from: seller
-    });
-    await marketplace.addImageForItemForSale(itemIndex, item.imageIpfsHashes[1], {
-      from: seller
-    });
-  }
-  const getBalance = async account => await web3.eth.getBalance(owner).toNumber() 
-  const seedSellerItemsForSaleBy = async (itemsForSaleCountSeed) => {
+    await marketplace.addImageForItemForSale(
+      itemIndex,
+      item.imageIpfsHashes[0],
+      {
+        from: seller
+      }
+    );
+    await marketplace.addImageForItemForSale(
+      itemIndex,
+      item.imageIpfsHashes[1],
+      {
+        from: seller
+      }
+    );
+  };
+  const getBalance = async account =>
+    await web3.eth.getBalance(owner).toNumber();
+  const seedSellerItemsForSaleBy = async itemsForSaleCountSeed => {
     for (let index = 0; index < itemsForSaleCountSeed; index++) {
-      await postItemForSale();      
+      await postItemForSale();
     }
-  }
+  };
 
   beforeEach(async () => {
     marketplace = await Marketplace.new();
-    await marketplace.registerUser(buyerData.role, buyerData.email, buyerData.number, {
+    await marketplace.registerUser(
+      buyerData.role,
+      buyerData.email,
+      buyerData.number,
+      {
         from: buyer
-      });
-    await marketplace.registerUser(sellerData.role, sellerData.email, sellerData.number, {
+      }
+    );
+    await marketplace.registerUser(
+      sellerData.role,
+      sellerData.email,
+      sellerData.number,
+      {
         from: seller
-      });
+      }
+    );
   });
 
   it("does not allow visitors/unregistered to view the seller's contact information", async () => {
@@ -71,7 +84,9 @@ contract("Marketplace", accounts => {
 
   it("allows sellers to add item for sale", async () => {
     await postItemForSale();
-    const sellerItemForSaleCountActual = await marketplace.getItemForSaleCount(seller);
+    const sellerItemForSaleCountActual = await marketplace.getItemForSaleCount(
+      seller
+    );
     const sellerItemForSaleCountExpected = 1;
 
     assert.equal(sellerItemForSaleCountActual, sellerItemForSaleCountExpected);
@@ -108,19 +123,25 @@ contract("Marketplace", accounts => {
     const itemBoughtArr = await marketplace.getItemBought(0, {
       from: buyer
     });
-    const itemBoughtImageCountBigNum = await marketplace.getItemBoughtImageCount(0, {
-      from: buyer
-    });
+    const itemBoughtImageCountBigNum = await marketplace.getItemBoughtImageCount(
+      0,
+      {
+        from: buyer
+      }
+    );
     const itemBoughtImageCount = itemBoughtImageCountBigNum.toNumber();
     const imageIpfsHashes = [];
     for (let index = 0; index < itemBoughtImageCount; index++) {
-      const itemBoughtImageIpfsHash = await marketplace.getItemBoughtImage(0, index, {
-        from: buyer
-      });
-      
+      const itemBoughtImageIpfsHash = await marketplace.getItemBoughtImage(
+        0,
+        index,
+        {
+          from: buyer
+        }
+      );
+
       imageIpfsHashes.push(itemBoughtImageIpfsHash);
     }
-
 
     const itemBoughtActual = {
       name: itemBoughtArr[0],
@@ -143,7 +164,7 @@ contract("Marketplace", accounts => {
     const commissionActual = await marketplace.viewCommission({
       from: owner
     });
-    const commissionExpected = item.price * 5 / 100;
+    const commissionExpected = (item.price * 5) / 100;
 
     assert.equal(commissionActual.toNumber(), commissionExpected);
   });
@@ -156,35 +177,46 @@ contract("Marketplace", accounts => {
     });
 
     await marketplace.cashoutCommission();
-    
+
     const marketplaceCommissionBigNum = await marketplace.viewCommission({
       from: owner
     });
     const marketplaceCommission = marketplaceCommissionBigNum.toNumber();
-    
+
     assert.equal(marketplaceCommission, 0);
   });
 
   it("allows visitors to view items for sale", async () => {
     const itemsForSaleSeedCount = 2;
     await seedSellerItemsForSaleBy(itemsForSaleSeedCount);
-    
-    const sellerItemsForSaleCountBigNum = await marketplace.getItemForSaleCount(seller, {
-      from: visitor
-    });
+
+    const sellerItemsForSaleCountBigNum = await marketplace.getItemForSaleCount(
+      seller,
+      {
+        from: visitor
+      }
+    );
     const sellerItemsForSaleCount = sellerItemsForSaleCountBigNum.toNumber();
 
     const sellerItemsForSale = [];
     for (let itemIndex = 0; itemIndex < sellerItemsForSaleCount; itemIndex++) {
-      const itemForSaleArr = await marketplace.getItemForSale(seller, itemIndex, {
-        from: visitor
-      });
-      const itemForSaleImageCountBigNum = await marketplace.getItemForSaleImageCount(seller, itemIndex, {
-        from: visitor
-      });
+      const itemForSaleArr = await marketplace.getItemForSale(
+        seller,
+        itemIndex,
+        {
+          from: visitor
+        }
+      );
+      const itemForSaleImageCountBigNum = await marketplace.getItemForSaleImageCount(
+        seller,
+        itemIndex,
+        {
+          from: visitor
+        }
+      );
       const itemForSaleImageCount = itemForSaleImageCountBigNum.toNumber();
 
-      sellerItemsForSale.push({ 
+      sellerItemsForSale.push({
         name: itemForSaleArr[0],
         description: itemForSaleArr[1],
         price: itemForSaleArr[2].toNumber(),
@@ -192,11 +224,18 @@ contract("Marketplace", accounts => {
       });
 
       for (let imgIndex = 0; imgIndex < itemForSaleImageCount; imgIndex++) {
-        const itemForSaleImageIpfsHash = await marketplace.getItemForSaleImage(seller, itemIndex, imgIndex, {
-          from: visitor
-        });
+        const itemForSaleImageIpfsHash = await marketplace.getItemForSaleImage(
+          seller,
+          itemIndex,
+          imgIndex,
+          {
+            from: visitor
+          }
+        );
 
-        sellerItemsForSale[itemIndex].imageIpfsHashes.push(itemForSaleImageIpfsHash);
+        sellerItemsForSale[itemIndex].imageIpfsHashes.push(
+          itemForSaleImageIpfsHash
+        );
       }
     }
 
@@ -212,9 +251,15 @@ contract("Marketplace", accounts => {
       description: "1 Liter",
       price: 5 * FINNEY
     };
-    await marketplace.updateItemForSale(0, newItem.name, newItem.description, newItem.price, {
-      from: seller
-    });
+    await marketplace.updateItemForSale(
+      0,
+      newItem.name,
+      newItem.description,
+      newItem.price,
+      {
+        from: seller
+      }
+    );
 
     const updatedItemArr = await marketplace.getItemForSale(seller, 0, {
       from: seller
@@ -230,57 +275,82 @@ contract("Marketplace", accounts => {
 
   it("allows seller to add image for their item for sale", async () => {
     await postItemForSale();
-    const itemForSaleImageCountBigNumExpected = await marketplace.getItemForSaleImageCount(seller, 0, {
-      from: seller
-    });
-  
+    const itemForSaleImageCountBigNumExpected = await marketplace.getItemForSaleImageCount(
+      seller,
+      0,
+      {
+        from: seller
+      }
+    );
+
     await marketplace.addImageForItemForSale(0, item.imageIpfsHashes[0], {
       from: seller
     });
 
-    const itemForSaleImageCountBigNumActual = await marketplace.getItemForSaleImageCount(seller, 0, {
-      from: seller
-    });
+    const itemForSaleImageCountBigNumActual = await marketplace.getItemForSaleImageCount(
+      seller,
+      0,
+      {
+        from: seller
+      }
+    );
     const itemForSaleImageCountActual = itemForSaleImageCountBigNumActual.toNumber();
-    const itemForSaleImageCountExpected = itemForSaleImageCountBigNumExpected.toNumber() + 1;
+    const itemForSaleImageCountExpected =
+      itemForSaleImageCountBigNumExpected.toNumber() + 1;
 
     assert.equal(itemForSaleImageCountActual, itemForSaleImageCountExpected);
   });
 
   it("allows seller to remove their item for sale", async () => {
     await postItemForSale();
-    const itemForSaleCountBigNumExpected = await marketplace.getItemForSaleCount(seller, {
-      from: seller
-    });
-    
+    const itemForSaleCountBigNumExpected = await marketplace.getItemForSaleCount(
+      seller,
+      {
+        from: seller
+      }
+    );
+
     await marketplace.removeItemForSaleBySeller(0, {
       from: seller
     });
 
-    const itemForSaleCountBigNumActual = await marketplace.getItemForSaleCount(seller, {
-      from: seller
-    });
-    const itemForSaleCountActual = itemForSaleCountBigNumActual.toNumber();    
-    const itemForSaleCountExpected = itemForSaleCountBigNumExpected.toNumber() - 1;
+    const itemForSaleCountBigNumActual = await marketplace.getItemForSaleCount(
+      seller,
+      {
+        from: seller
+      }
+    );
+    const itemForSaleCountActual = itemForSaleCountBigNumActual.toNumber();
+    const itemForSaleCountExpected =
+      itemForSaleCountBigNumExpected.toNumber() - 1;
 
     assert.equal(itemForSaleCountActual, itemForSaleCountExpected);
   });
 
   it("allows seller to remove an image for their item for sale", async () => {
     await postItemForSale();
-    const itemForSaleImageCountBigNumExpected = await marketplace.getItemForSaleImageCount(seller, 0, {
-      from: seller
-    });
+    const itemForSaleImageCountBigNumExpected = await marketplace.getItemForSaleImageCount(
+      seller,
+      0,
+      {
+        from: seller
+      }
+    );
 
     await marketplace.removeItemForSaleImage(0, 0, {
       from: seller
     });
 
-    const itemForSaleImageCountBigNumActual = await marketplace.getItemForSaleImageCount(seller, 0, {
-      from: seller
-    });
+    const itemForSaleImageCountBigNumActual = await marketplace.getItemForSaleImageCount(
+      seller,
+      0,
+      {
+        from: seller
+      }
+    );
     const itemForSaleImageCountActual = itemForSaleImageCountBigNumActual.toNumber();
-    const itemForSaleImageCountExpected = itemForSaleImageCountBigNumExpected.toNumber() - 1;
+    const itemForSaleImageCountExpected =
+      itemForSaleImageCountBigNumExpected.toNumber() - 1;
 
     assert.equal(itemForSaleImageCountActual, itemForSaleImageCountExpected);
   });
@@ -308,9 +378,12 @@ contract("Marketplace", accounts => {
       const itemBoughtArr = await marketplace.getItemBought(itemIndex, {
         from: buyer
       });
-      const itemBoughtImageCountBigNum = await marketplace.getItemBoughtImageCount(itemIndex, {
-        from: buyer
-      });
+      const itemBoughtImageCountBigNum = await marketplace.getItemBoughtImageCount(
+        itemIndex,
+        {
+          from: buyer
+        }
+      );
       const itemBoughtImageCount = itemBoughtImageCountBigNum.toNumber();
 
       itemsBought.push({
@@ -321,9 +394,13 @@ contract("Marketplace", accounts => {
       });
 
       for (let imgIndex = 0; imgIndex < itemBoughtImageCount; imgIndex++) {
-        const imageIpfsHash = await marketplace.getItemBoughtImage(itemIndex, imgIndex, {
-          from: buyer
-        });
+        const imageIpfsHash = await marketplace.getItemBoughtImage(
+          itemIndex,
+          imgIndex,
+          {
+            from: buyer
+          }
+        );
 
         itemsBought[itemIndex].imageIpfsHashes.push(imageIpfsHash);
       }
@@ -366,9 +443,11 @@ contract("Marketplace", accounts => {
   });
 
   it("allows anyone to view sellers address", async () => {
-    const userAddressIndicesCountBigNum = await marketplace.getUserAddressIndicesCount({
-      from: visitor
-    });
+    const userAddressIndicesCountBigNum = await marketplace.getUserAddressIndicesCount(
+      {
+        from: visitor
+      }
+    );
     const userAddressIndicesCount = userAddressIndicesCountBigNum.toNumber();
 
     const registeredUsers = [];
@@ -377,7 +456,7 @@ contract("Marketplace", accounts => {
         from: visitor
       });
 
-      registeredUsers.push(userAddress)
+      registeredUsers.push(userAddress);
     }
 
     const registeredUsersExpected = [buyer, seller];
@@ -439,4 +518,32 @@ contract("Marketplace", accounts => {
       assert.isOk(/revert/.test(err.message));
     }
   });
-})
+
+  it("allows user to get their role", async () => {
+    const roleIndexBigNum = await marketplace.getRole({
+      from: seller
+    });
+    const roleIndex = roleIndexBigNum.toNumber();
+    const expected = 1;
+
+    assert.equal(roleIndex, expected);
+  });
+
+  it("allows users to update their contact", async () => {
+    const newContact = {
+      email: 'new@a.com',
+      number: '12345'
+    };
+    
+    await marketplace.updateContact(newContact.email, newContact.number, {
+      from: seller
+    });
+    const sellerContactArr = await marketplace.getSellerContact(seller, { from: seller });
+    const newContactActual = {
+      email: sellerContactArr[0],
+      number: sellerContactArr[1]
+    };
+
+    assert.deepEqual(newContactActual, newContact);
+  });
+});
